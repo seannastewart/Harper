72221861f08e684d9d4e3e3a564126213fbf2d7e
seannastewart <seannastew@gmail.com>
2022-02-04 16:19:53 -0500

    added button submitted feature

--- scripts.js
+++ scripts.js
@@ -1,17 +1,26 @@
 const navElement = document.querySelector('nav');
 const ulElement = document.querySelector('ul');
 const menuIcon = document.querySelector('i');
-console.log(menuIcon)
+
 
 
 navElement.addEventListener("click", function (event) {
 
     if (event.target.tagName === 'I') {
-        // tasks to perform
-        console.log('wefwfwg');
+
         ulElement.classList.toggle('mobileNav');
     }
 
 });
 
+const formElement = document.querySelector('form');
+const buttonElement = document.querySelector('button');
+
+
+formElement.addEventListener('submit', function (event) {
+
+    event.preventDefault();
+    buttonElement.classList.add('submitted')
+    buttonElement.textContent = "Submitted"
+});
 
